7fb2311cc01f90b60f737ccb8bbff953793fa8ec
projas23 <pedrojas23@gmail.com>
2024-01-15 19:40:10 -0800

    added patients.jsos file, changes to patient component

--- patientpath-frontend/src/App.js
+++ patientpath-frontend/src/App.js
@@ -7,8 +7,19 @@ import Appointments from "./components/shared/Appoinments";
 import Scheduling from "./components/shared/Scheduling";
 import Dashboard from "./components/shared/Dashboard";
 import Notifications from "./components/shared/Notifications";
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    fetch('/patients.json')
+      .then(response => response.json())
+      .then(data => setPatients(data))
+      .catch(error => console.error('Error fetching patients data:', error));
+  }, []);
+
+
   return (
     <div>
       {/* <h1>Patient Path</h1> */}
@@ -16,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/admin" element={<AdminScreen />}/>
         <Route path="/providers" element={<ProviderList />}/>
-        <Route path="/patients" element={<PatientList />}/>
+        <Route path="/patients" element={<PatientList patients={patients} />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
         <Route path="/appointments"> 
           <Route index path="/appointments" element={<Appointments/> }/>
